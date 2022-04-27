d4387a7a8c075cbc4defd6ad4223c9fb6ebd2f1e
Bhavya <f20191404@hyderabad.bits-pilani.ac.in>
2022-04-27 23:20:36 +0530

    submission

---
+++ hospital/aboutus.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Developer</title>
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Zilla+Slab+Highlight&display=swap" rel="stylesheet">
+    <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Kiwi+Maru:wght@300&display=swap" rel="stylesheet">
+
+      <link rel="stylesheet" href="styles/about-us.css">
+  </head>
+  <body style="background-image: linear-gradient(to right, #000d26 0%, #6074d6 86%);;">
+    <center>
+
+        <h1 style="font-family: 'Times New Roman', cursive; margin:50px auto 0px; color:white; font-size:50px;">Meet the Team</h1>
+
+
+    </center>
+<div style="margin:150px 0px 0px 75px;">
+<figure class="snip1515" position ="relative"  style="display:inline-block;">
+
+  <figcaption>
+      <h3>Mohak Kapil</h3>
+
+    <p>2019B3A70680H</p>
+
+  </figcaption>
+</figure>
+<figure class="snip1515" style="display:inline-block;">
+  
+  <figcaption>
+    <h3>Kanishk Yadav </h3>
+
+    
+
+        <p>2019B2A71452H</p>
+
+    
+  </figcaption>
+</figure>
+<figure class="snip1515" style="display:inline-block;">
+  
+  <figcaption>
+    <h3>Bhavya Tibrewala</h3>
+    
+    <p>2019B2A71404H</p>
+
+    
+  </figcaption>
+</figure>
+
+<figure class="snip1515" style="display:inline-block;">
+  
+  <figcaption>
+    <h3>Rohita Sai</h3>
+    
+
+        <p>2019B2A70059H</p>
+
+       
+
+
+              
+
+        
+  </figcaption>
+</figure>
+</div>
+
+<script type="text/javascript">
+$(".hover").mouseleave(
+function () {
+  $(this).removeClass("hover");
+}
+);
+</script>
+
+</body>
+</html>
